94b6b73bb7b323798773f98a2eb0b76d49d75929
hkawoosa <hkawoosa@gmail.com>
2017-03-11 10:52:26 -0500

    added girl interactions

---
+++ Assets/Entities/Girl/Interactons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactons : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("Insert bullshit interaction here :'(");
+        }
+
+        if (other.tag == "Flower")
+        {
+            print("Insert interaction here :D");
+        }
+    }
+}
