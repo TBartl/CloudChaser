003b9e7e8da32b6b8dfd11b47e50d8790591e45f
TBartl <tbartl@umich.edu>
2017-03-11 10:48:32 -0500

    MEMES

--- Assets/Misc/GameManager.cs
+++ Assets/Misc/GameManager.cs
@@ -8,6 +8,9 @@ public struct IntroSequenceSettings {
     public float spaceSpeedUpMultiplier;
     public float flowerToGirlTime;
     public AnimationCurve flowerToGirlCurve;
+    
+    public float girlToPlayerTimeTime;
+    public AnimationCurve girlToPlayerTimeCurve;
 
     public Vector3 flowerRotation;
     public Vector3 girlRotation;
@@ -42,6 +45,14 @@ public class GameManager : MonoBehaviour {
             p = intro.flowerToGirlCurve.Evaluate(p);
             Camera.main.transform.position = Vector3.Lerp(flower.position + offset, girl.position + offset, p);
             Camera.main.transform.rotation = Quaternion.Slerp(Quaternion.Euler(intro.flowerRotation), Quaternion.Euler(intro.girlRotation), p);
+            yield return null;
+        }
+
+        for (float t = 0; t < intro.girlToPlayerTimeTime; t += (Input.GetKey(KeyCode.Space) ? Time.deltaTime * intro.spaceSpeedUpMultiplier : Time.deltaTime)) {
+            float p = (t / intro.girlToPlayerTimeTime);
+            p = intro.girlToPlayerTimeCurve.Evaluate(p);
+            Camera.main.transform.position = Vector3.Lerp(girl.position + offset, player.transform.position, p);
+            Camera.main.transform.rotation = Quaternion.Slerp(Quaternion.Euler(intro.girlRotation), Quaternion.Euler(Vector3.zero), p);
             yield return null;
         }
 
