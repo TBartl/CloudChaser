8cb7b8595706c0a732ccea7ecd1eb73f385d46f3
TBartl <tbartl@umich.edu>
2017-03-10 19:36:23 -0500

    view

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    PlayerView view;
+    public AnimationCurve accelerationBySpeed;
+    public float decceleration = 5f;
+
+    [HideInInspector] public Rigidbody rigid;
+
+	// Use this for initialization
+	void Awake () {
+        view = this.GetComponentInChildren<PlayerView>();
+        rigid = this.GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
+        Vector3 moveDirection = view.transform.TransformVector(inputDirection);
+        moveDirection.y = 0;
+        moveDirection =  moveDirection.normalized;
+
+        Vector3 currentHorizontalVelocity = rigid.velocity;
+        currentHorizontalVelocity.z = 0;
+        rigid.AddForce(moveDirection * accelerationBySpeed.Evaluate(currentHorizontalVelocity.magnitude)  * Time.deltaTime);
+
+	}
+}
---
+++ Assets/PlayerView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour {
+
+    Transform mainCamera;
+    Vector3 rotation;
+    public float sensitivity = 20f;
+
+    void Start() {
+        mainCamera = Camera.main.transform;
+        UpdateCameraPosition();
+    }
+
+	// Update is called once per frame
+	void LateUpdate () {
+        rotation += new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0) * sensitivity; // Don't use time.deltaTime
+        rotation.x = Mathf.Clamp(rotation.x, -90, 90);
+        this.transform.rotation = Quaternion.Euler(rotation);
+        UpdateCameraPosition();
+	}
+
+    void UpdateCameraPosition() {
+        mainCamera.transform.position = this.transform.position;
+        mainCamera.transform.rotation = this.transform.rotation;
+    }
+}
