0c8ff848415842faa65f281d003f90b4bbe81dda
hkawoosa <hkawoosa@gmail.com>
2017-03-11 10:32:05 -0500

    Made flower collectible

---
+++ Assets/Collect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            this.transform.SetParent(other.transform);
+            this.transform.localPosition = Vector3.up;
+        }
+    }
+}
