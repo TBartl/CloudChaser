873ac67fca4aaeede956ec906185ae3b549321f3
hkawoosa <hkawoosa@gmail.com>
2017-03-11 03:41:18 -0500

    added delayed destroyed clouds

---
+++ Assets/World/Crumble.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crumble : MonoBehaviour {
+    public float delay = 2.0f;
+	
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag.Equals("Player"))
+        {
+            Destroy(this.gameObject, delay);
+        }
+    }
+
+}
